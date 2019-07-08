f533a664c45aa36d903f63005b3f1313578cc13e
Alvarado <e.alvarado@accenture.com>
2019-07-08 09:07:16 -0500

    Second Commit

---
+++ Payments/WebPaymentsApi/Models/PaymentDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPaymentsApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Column(TypeName = "varchar(16)")]
+
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+
+        public string ExpirationDate { get; set; }
+        [Required]
+        public int CVV { get; set; }
+    }
+}
